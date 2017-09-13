a6d200512ae5e33958fa738446444f8b0eb9adc6
mikko <mikko@DESKTOP-I5LP193.wlan.metropolia.fi>
2017-09-13 10:26:48 +0300

    testi

--- AMKF/src/amkf/AMKF.java
+++ AMKF/src/amkf/AMKF.java
@@ -20,7 +20,7 @@ public class AMKF extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+        //asdasdasdasdasd
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
