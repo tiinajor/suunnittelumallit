120ad8c895364a631f812f3b97f4302e1e26845c
minna <minna@MSI.wlan.metropolia.fi>
2017-09-27 12:52:19 +0300

    Ulkoasua

--- AMKF/src/amkf/AMKF.java
+++ AMKF/src/amkf/AMKF.java
@@ -33,7 +33,7 @@ public class AMKF extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        root = FXMLLoader.load(getClass().getResource("indexMusta.fxml"));
+        root = FXMLLoader.load(getClass().getResource("index.fxml"));
 
 
 
