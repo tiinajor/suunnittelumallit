c8c0587d459899522921a5396694a451b27770cf
Smuli <samuli.kakonen@metropolia.fi>
2017-11-02 14:24:27 +0200

    Testei2

--- AMKF/test/Controller/LoginControllerTest.java
+++ AMKF/test/Controller/LoginControllerTest.java
@@ -55,12 +55,11 @@ public class LoginControllerTest {
         //instance.initialize(url, rb);
 
         String[] maakunnat = kone.getAsuinalueet();
-        
+        System.out.println("asdd");
         assertEquals("Maakuntien haku epäonnistui", maakunnat != null);
         
         System.out.println("Eka testi");
         // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
     }
 
     /**
